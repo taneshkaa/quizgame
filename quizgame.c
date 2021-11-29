2b6b034ad6c3a94526ce5d9a8f0ccab4c477fe13
taneshkaa <85098642+taneshkaa@users.noreply.github.com>
2021-11-29 16:43:42 +0530

    Add files via upload

---
+++ quizgame.c
@@ -0,0 +1,527 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
+int main()
+     {
+     int countr,r,r1,count,i,n;
+     float score;
+     char choice;
+     char playername[20];
+     mainhome:
+     system("cls");
+     printf("\t\t\tC PROGRAM QUIZ GAME\n");
+     printf("\n\t\t________________________________________");
+
+     printf("\n\t\t\t   WELCOME ");
+     printf("\n\t\t\t      to ");
+     printf("\n\t\t\t   THE GAME ");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t   EXPLORE AND SCORE 1000000!!    ") ;
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t > Press S to start the game");
+     printf("\n\t\t > Press V to view the highest score  ");
+     printf("\n\t\t > Press R to reset score");
+     printf("\n\t\t > press H for help            ");
+     printf("\n\t\t > press Q to quit             ");
+     printf("\n\t\t________________________________________\n\n");
+     choice=toupper(getch());
+     if (choice=='V')
+	{
+	show_record();
+	goto mainhome;
+	}
+     else if (choice=='H')
+	{
+	help();getch();
+	goto mainhome;
+	}
+	else if (choice=='R')
+	{reset_score();
+	getch();
+	goto mainhome;}
+	else if (choice=='Q')
+	exit(1);
+    else if(choice=='S')
+    {
+     system("cls");
+
+    printf("\n\n\n\n\n\n\n\n\n\n\t\t\t Type Your Name:");
+     gets(playername);
+
+    system("cls");
+    printf("\n ------------------  Welcome %s to C Program Quiz Game --------------------------",playername);
+    printf("\n\n Guide before staring the game:");
+    printf("\n -------------------------------------------------------------------------");
+    printf("\n >> There are 2 rounds in this Quiz Game, BASIC ROUND & CHALLANGE ROUND");
+    printf("\n >> In basic round you will be asked a total of 3 questions to test your");
+    printf("\n    general knowledge. You are eligible to play the game if you give atleast 2");
+    printf("\n    right answers, otherwise you can't proceed further to the Challenge Round.");
+    printf("\n >> Your game starts with CHALLANGE ROUND. In this round you will be asked");
+    printf("\n    total of 10 questions. Each right answer will be awarded with points.");
+    printf("\n    By this way scoring the maximum score, you can win !!!!!..........");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option.");
+    printf("\n >> You will be asked questions continuously, till right answers are given");
+    printf("\n >> No negative marking for wrong answers!");
+    printf("\n\n\t!!!!!!!!!!!!! GOOD LUCK !!!!!!!!!!!!!");
+    printf("\n\n\n Press Y  to start the game!\n");
+    printf("\n Press any other key to return to the main menu!");
+    if (toupper(getch())=='Y')
+		{
+		    goto home;
+        }
+	else
+		{
+        goto mainhome;
+       system("cls");
+       }
+
+     home:
+     system("cls");
+     count=0;
+     for(i=1;i<=3;i++)
+     {
+    system("cls");
+     r1=i;
+
+
+     switch(r1)
+		{
+		case 1:
+		printf("\n\nWhich of the following is a Palindrome number?");
+		printf("\n\nA.42042\t\tB.101010\n\nC.23232\t\tD.01234");
+		if (toupper(getch())=='C')
+			{
+			    printf("\n\nCorrect!!!");count++;
+			    getch();
+			    break;
+}
+		else
+		       {
+		           printf("\n\nWrong!!! The correct answer is C.23232");
+		           getch();
+		       break;
+		       }
+
+        case 2:
+		printf("\n\n\nWhich email service is owned by Microsoft?");
+		printf("\n\nA.Gmail\t\tB.Yahoo\n\nC.Hotmail\t\tD.Rediffmail");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.Hotmail");
+		       getch();
+		       break;}
+
+        case 3:
+		printf("\n\n\nWhich animal laughs like human being?");
+		printf("\n\nA.Polar Bear\t\tB.Hyena\n\nC.Donkey\t\tD.Chimpanzee");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.Hyena");
+		       getch();
+		       break;}
+
+        case 4:
+		printf("\n\n\nHow many days does it take for the Earth to orbit the Sun?");
+		printf("\n\nA.367\t\tB.365\n\nC.365 1/2\t\tD.366");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.365");
+		       getch();
+		       break;}
+
+        case 5:
+        printf("\n\n\nWhich is the third highest mountain in the world?");
+        printf("\n\nA.Mt. K2\t\tB.Mt. Kanchanjungha\n\nC.Mt. Makalu\t\tD.Mt. Kilimanjaro");
+        if (toupper(getch())=='B')
+               {printf("\n\nCorrect!!!");count++;
+               getch();
+                break;}
+        else
+		       {printf("\n\nWrong!!! The correct answer is B.Mt. Kanchanjungha");
+		       getch();
+		       break;}
+
+        case 6:
+		printf("\n\n\nHow many pairs of wings does a bee have?");
+		printf("\n\nA.One\t\tB.Three\n\nC.Five\t\tD.Two");
+		if (toupper(getch())=='D' )
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.Two");
+		       getch();
+		       break;}}
+		       }
+
+	if(count>=2)
+	{goto test;}
+	else
+	{
+	system("cls");
+	printf("\n\nSORRY YOU ARE NOT ELIGIBLE TO PLAY THIS GAME, BETTER LUCK NEXT TIME");
+	getch();
+	goto mainhome;
+	}
+     test:
+     system("cls");
+     printf("\n\n\t*** CONGRATULATION %s you are eligible to play the Game ***",playername);
+     printf("\n\n\n\n\t!Press any key to Start the Game!");
+     if(toupper(getch())=='p')
+		{goto game;}
+game:
+     countr=0;
+     for(i=1;i<=10;i++)
+     {system("cls");
+     r=i;
+
+     switch(r)
+		{
+		case 1:
+		printf("\n\nWhat is the National Game of England?");
+		printf("\n\nA.Football\t\tB.Basketball\n\nC.Cricket\t\tD.Baseball");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;getch();}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.Cricket");getch();
+		       goto score;
+		       break;}
+
+		case 2:
+		printf("\n\n\nHow many phases of the moon are there?");
+		printf("\n\nA.Eight\t\tB.Sixteen\n\nC.Five\t\tD.Eleven");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.Eight");getch();
+		      goto score;
+		       break;
+		       }
+
+        case 3:
+		printf("\n\n\nHow many hearts does an octopus have? ");
+		printf("\n\nA.Four\t\tB.One\n\nC.Three\t\tD.Two");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.Three");getch();
+		       goto score;
+		       break;}
+
+        case 4:
+		printf("\n\n\nThe Laws of Electromagnetic Induction were given by?");
+		printf("\n\nA.Faraday\t\tB.Tesla\n\nC.Maxwell\t\tD.Coulomb");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {
+                printf("\n\nWrong!!! The correct answer is A.Faraday");getch();
+		       goto score;
+		       break;
+		       }
+
+        case 5:
+		printf("\n\n\nIn what unit is electric power measured?");
+		printf("\n\nA.Coulomb\t\tB.Watt\n\nC.Power\t\tD.Units");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");countr++;getch(); break;}
+		else
+		       {
+		           printf("\n\nWrong!!! The correct answer is B.Power");
+		       getch();
+		       goto score;
+		       break;
+		       }
+
+		case 6:
+		printf("\n\n\nHow many total time zones are there in the world?");
+		printf("\n\nA.22\t\tB.24\n\nC.31\t\tD.30");
+		if (toupper(getch())=='B' )
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.24");goto score;
+		       getch();
+		       break;}
+
+        case 7:
+		printf("\n\n\nWhich was the first living creature sent in space?");
+		printf("\n\nA.Rat\t\tB.Monkey\n\nC.Cat\t\tD.Dog");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.Dog");getch();
+		       goto score;
+		       break;}
+
+        case 8:
+		printf("\n\n\nHow many times a piece of paper can be folded at the most?");
+		printf("\n\nA.6\t\tB.7\n\nC.8\t\tD.Depends on the size of paper");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");countr++;getch(); break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.7");getch();
+		       goto score;
+		       break;}
+
+        case 9:
+		printf("\n\n\nWho was the father of Indian Space Programme?");
+		printf("\n\nA.Dr. Vikram Ambalal Sarabhai\t\tB.Dr. Satish Dhawan\n\nC.Dr. Homi Bhabha\t\tD.Dr. Krishnaswami Kasturirangan");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.Dr. Vikram Ambalal Sarabhai");getch();
+		       goto score;
+		       break;}
+
+        case 10:
+		printf("\n\n\nWhich is the longest River in the world?");
+		printf("\n\nA.Nile\t\tB.Koshi\n\nC.Ganga\t\tD.Amazon");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++;getch(); break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.Nile");getch();break;goto score;}
+
+        case 11:
+		printf("\n\n\nWhat is the color of the Black Box in aeroplanes?");
+		printf("\n\nA.White\t\tB.Black\n\nC.Orange\t\tD.Red");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+              {printf("\n\nWrong!!! The correct answer is C.Orange");getch();
+              break;goto score;}
+
+        case 12:
+		printf("\n\n\nWhat is the name of the new dwarf planet which was discovered in 2005?");
+		printf("\n\nA.Eris\t\tB.Erin\n\nC.Eric\t\tD.Eros");
+		if (toupper(getch())=='A')
+			  {printf("\n\nCorrect!!!");countr++;getch();
+			   break;}
+		else
+              {printf("\n\nWrong!!! The correct answer is A.Eris");getch();
+              break;goto score;}
+
+		case 13:
+		printf("\n\n\nName the country where there no mosquitoes are found?");
+		printf("\n\nA.Japan\t\tB.Italy\n\nC.Argentina\t\tD.France");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.France");getch();
+		       break;goto score;}
+
+        case 14:
+		printf("\n\n\nWho discovered C Programming Language?");
+		printf("\n\nA.Dennis Ritchie\t\tB.Rasmus Lerdorf\n\nC.Larry Wall\t\tD.James Gosling");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.Dennis Ritche");getch();
+		       break;goto score;}
+
+		case 15:
+		printf("\n\n\nWhat is the full form of CSS?");
+		printf("\n\nA.Cascading Spread Sheets\t\tB.Cascading Style Sheets\n\nC.Classy Style Sheets\t\tD.Classic Style Sheets");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.Cascading Style Sheets");getch();
+		       goto score;
+		       break;}
+
+		case 16:
+		printf("\n\n\nWhat is the unit of measurement of distance between Stars?");
+		printf("\n\nA.Light Year\t\tB.Coulomb\n\nC.Nautical Mile\t\tD.Kilometer");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;
+			}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.Light Year");getch();
+		       goto score;
+		       break;}
+
+
+		case 17:
+		printf("\n\n\n Who discovered 'Big Data'");
+		printf("\n\nA.Roger Mougalas\t\tB.Dennis Ritchie\n\nC.Larry Walls\t\tD.Ross Ihaka");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.Roger Mougalas");getch();goto score;
+		       break;}
+
+		case 18:
+		printf("\n\n\nWind speed is measure by__________?");
+		printf("\n\nA.Lysimeter\t\tB.Air vane\n\nC.Hydrometer\t\tD.Anemometer\n\n");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.Anemometer");getch();goto score;
+		       break;}
+
+		case 19:
+		printf("\n\n\nWhich city in the world is popularly known as The City of Temple?");
+		printf("\n\nA.Delhi\tB.Bhaktapur\n\nC.Kathmandu\tD.Agra\n\n");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.Kathmandu");getch();goto score;
+		       break;}
+
+		case 20:
+		printf("\n\n\nWhich hardware was used in the First Generation Computer?");
+		printf("\n\nA.Transistor\t\tB.Valves\n\nC.I.C\t\tD.S.S.I");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.Valves");getch();goto score;
+		       break;}
+
+		case 21:
+		printf("\n\n\nOzone plate is being destroyed regularly because of____ ?");
+		printf("\n\nA.L.P.G\t\tB.Nitrogen\n\nC.Methane\t\tD. C.F.C");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D. C.F.C");getch();goto score;
+		       break;}
+
+		case 22:
+		printf("\n\n\nWhich is the only number that cannot be used as a divisor?");
+		printf("\n\nA.2\t\tB.9\n\nC.1\t\tD.0");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.0");getch();goto score;
+		       break;}
+
+		case 23:
+		printf("\n\n\nWhen is Earth Day observed?");
+		printf("\n\nA.22 May\t\tB.24 April\n\nC.22 April\t\tD.24 May");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.22 April");getch();goto score;
+		       break;}}}
+	score:
+    system("cls");
+	score=(float)countr*100000;
+	if(score>0.00 && score<1000000)
+	{
+	   printf("\n\n\t\t**************** CONGRATULATION *****************");
+	     printf("\n\t You won, score= %.2f",score);goto go;}
+
+	 else if(score==1000000.00)
+	{
+	    printf("\n\n\n \t\t**************** CONGRATULATION ****************");
+	    printf("\n\t\t Your score is %.2f",score);
+	    printf("\t\t Thank You!!");
+	}
+	 else
+{
+	 printf("\n\n\t******** SORRY YOU DIDN'T WIN THE HIGHEST SCORE********");
+	    printf("\n\t\t Thanks for your participation");
+	    printf("\n\t\t TRY AGAIN");goto go;}
+
+	go:
+	puts("\n\n Press Y if you want to play next game");
+	puts(" Press any key if you want to go main menu");
+	if (toupper(getch())=='Y')
+		goto home;
+	else
+		{
+		edit_score(score,playername);
+		goto mainhome;}}}
+
+void show_record()
+    {system("cls");
+	char name[20];
+	float scr;
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&name,&scr);
+	printf("\n\n\t\t*************************************************************");
+	printf("\n\n\t\t %s has secured the Highest Score %0.2f",name,scr);
+	printf("\n\n\t\t*************************************************************");
+	fclose(f);
+	getch();}
+
+void reset_score()
+    {system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r+");
+	fscanf(f,"%s%f",&nm,&sc);
+	sc=0;
+	fprintf(f,"%s,%.2f",nm,sc);
+    fclose(f);}
+
+void help()
+	{system("cls");
+    printf("\n\n                              HELP");
+    printf("\n -------------------------------------------------------------------------");
+    printf("\n ......................... C program Quiz Game...........");
+    printf("\n >> There are two rounds in the game, BASIC ROUND & CHALLANGE ROUND");
+    printf("\n >> In basic round you will be asked a total of 3 questions to test your general");
+    printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
+    printf("\n    right answers otherwise you can't play the Game...........");
+    printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
+    printf("\n    total 10 questions each right answer will be awarded 100,000 scores.");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option");
+    printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
+    printf("\n >> No negative marking for wrong answers");
+
+	printf("\n\n\t*********************GOOD LUCK*********************************");
+	printf("\n\n\t*****C PROGRAM QUIZ GAME is developed by CODE WITH C TEAM********");}
+
+void edit_score(float score, char plnm[20])
+	{system("cls");
+	float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&nm,&sc);
+	if (score>=sc)
+	  { sc=score;
+	    fclose(f);
+	    f=fopen("score.txt","w");
+	    fprintf(f,"%s\n%.2f",plnm,sc);
+	    fclose(f);}}
